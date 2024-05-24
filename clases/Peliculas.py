f47f02ba83c9a7445bbb2ca8590afe3e4aa93abe
Serrenho Pfefferkorn Lucas Gonzalo <Lucas.Serrenho@supervielle.com.ar>
2024-05-24 10:07:35 -0300

    Cambios en la clase peliculas, se setea el index como id, y desaparece DateNorm y se utiliza de una Release_Date como la columna como data time

--- clases/Peliculas.py
+++ clases/Peliculas.py
@@ -10,7 +10,7 @@ caracteresComplejos=["'","-"]
 def genero_tabla_para_plot(df_tabla,generos):
 
     df_mov=df_tabla.copy()
-    df_mov['year'] = df_mov['DateNorm'].dt.year
+    df_mov['year'] = df_mov['Release_Date'].dt.year
     genre_columns = ['year']+ generos #Si no especificaron generos se muestran todos
     df_genres = df_mov[genre_columns].copy()
     genre_by_year = df_genres.groupby('year').sum()
@@ -55,34 +55,37 @@ class Pelicula:
         # Este método debe imprimir la información de esta película.
         return f'\n [{self.id}] {self.nombre} ({self.anio}) -{", ".join(map(str, self.generos))}-'
     
-    def write_df(self, df_mov):
+    def write_df(self, df):
         # Este método recibe el dataframe de películas y agrega la película
         # Si el id es None, toma el id más alto del DF y le suma uno. Si el 
         # id ya existe, no la agrega y devuelve un error.
-
+        df_mov=df.copy()
         if self.id == None:
-            self.id=df_mov['id'].max()+1
-        elif self.id in df_mov['id'].values:
+            self.id=df_mov.index.max()+1
+        elif self.id in df_mov.index:
             print('Error: No se pudo agregar, id ya existente')
             return df_mov
 
         generosdict = {a:1 for a in self.generos} # genero un dict con esta estructura {'Adventure': 1, 'Comedy': 1, 'Animation': 1}
-        new_row = {'id': self.id, 'Name': f'{self.nombre} ({self.anio})'}
+        new_row = {'Name': f'{self.nombre} ({self.anio})'}
         new_row.update(generosdict)
         TotalColumnasDF=df_mov.columns.tolist()
         RellenoConCerosDict={a : 0 for a in TotalColumnasDF if a not in new_row} # Completo el resto de los campos con 0
         new_row.update(RellenoConCerosDict)
+        df_mov.loc[self.id] = new_row
 
-        return df_mov.append(new_row, ignore_index=True)
+        return df_mov
 
-    def remove_from_df(self, df_mov):
+    def remove_from_df(self, df):
         # Borra del DataFrame el objeto contenido en esta clase.
         # Para realizar el borrado todas las propiedades del objeto deben coincidir
         # con la entrada en el DF. Caso contrario imprime un error.
+
+        df_mov= df.copy()
         
-        fila_a_borrar=self.get_from_df(df_mov, id=self.id, nombre = self.nombre, anios = [self.anio,self.anio], generos = self.generos)
+        fila_a_borrar=self.filtrar_df(df_mov, id=self.id, nombre = self.nombre, anios = [self.anio,self.anio], generos = self.generos)
         if len(fila_a_borrar)==1:
-            return df_mov.drop(df_mov[df_mov['id'] == fila_a_borrar[0].id].index)
+            return df_mov.drop(fila_a_borrar.index)
 
         else:
             print("No existe en el df recibido una película exactamente igual a la que invoca esta acción")
@@ -98,8 +101,10 @@ class Pelicula:
         ###
         df = pd.read_csv(filename)
         #Falta la validacion --> Hacerla por regex --> Hay algo más nativo ?
-        df['DateNorm'] = pd.to_datetime(df['Release Date'], format='%d-%b-%Y')
-        
+       
+        df.set_index('id', inplace=True)
+        df.columns = [col.replace(' ', '_') for col in df.columns]
+        df['Release_Date'] = pd.to_datetime(df['Release_Date'], format='%d-%b-%Y')
         df=faux.eliminoCaracterDeColumnasDF(df,["'","-"])
        
 
@@ -115,9 +120,8 @@ class Pelicula:
             # Crea un objeto Pelicula con los datos de la fila actual
             generos_activos = df_mov.loc[index].loc[df_mov.loc[index] == 1].index.tolist()
             nombre=row['Name'][:row['Name'].rfind(' (')]
-            # Una forma de calcular el año --> anio=int(row['Name'][row['Name'].rfind(' (')+2:-1])
-            anio2=row['DateNorm'].year # aprovechando que tenemos una columna dateTime
-            movie = Pelicula(nombre, anio2, generos_activos, id=row['id'])
+            anio=row['Release_Date'].year # aprovechando que tenemos una columna dateTime
+            movie = Pelicula(nombre, anio, generos_activos, id=index)
             # Agrega el objeto Pelicula a la lista
             lista_peliculas.append(movie)
 
@@ -135,7 +139,7 @@ class Pelicula:
 
         if anios!=None:
             desde,hasta=anios
-            queryText+='DateNorm >= "' + str(desde)+'-01-01' +'" and DateNorm <= "'+ str(hasta)+'-12-31' + '"'+ ' and '
+            queryText+='Release_Date >= "' + str(desde)+'-01-01' +'" and Release_Date <= "'+ str(hasta)+'-12-31' + '"'+ ' and '
 
         if generos!=None:
             generos = faux.elminoCaracterDeLista(generos,caracteresComplejos)# corrijo caracteres problematicos '-
@@ -164,22 +168,22 @@ class Pelicula:
         filtro = cls.filtrar_df(df_mov, anios = anios, generos= generos)
         print(f'-Cantidad de peliculas en la selección:{filtro.shape[0]}\n')
 
-        fecha_minima = filtro['DateNorm'].min()
-        PelisMasAntigua=filtro[filtro['DateNorm'] == fecha_minima]
+        fecha_minima = filtro['Release_Date'].min()
+        PelisMasAntigua=filtro[filtro['Release_Date'] == fecha_minima]
         if PelisMasAntigua.shape[0]==1:
             print(f'-Pelicula mas vieja:{cls.ConvertirAPeliculas(PelisMasAntigua)[0]}')
         else:
             print(f'-{PelisMasAntigua.shape[0]} películas comparten la fecha mas vieja:{cls.ConvertirAPeliculas(PelisMasAntigua)}')
 
-        fecha_maxima = filtro['DateNorm'].max()
-        PeliMasReciente=filtro[filtro['DateNorm'] == fecha_maxima]
+        fecha_maxima = filtro['Release_Date'].max()
+        PeliMasReciente=filtro[filtro['Release_Date'] == fecha_maxima]
         if PeliMasReciente.shape[0]==1:
             print(f'-\nPelicula mas reciente:\n{cls.ConvertirAPeliculas(PeliMasReciente)[0]}')
         else:
             print(f'-\n{PeliMasReciente.shape[0]} películas comparten la fecha mas reciente:{cls.ConvertirAPeliculas(PeliMasReciente)}')
         
         if generos==None:
-            generos=filtro.drop(columns=['DateNorm']).columns[5:].tolist()
+            generos=filtro.drop(columns=['Release_Date']).columns[5:].tolist()
         generos = faux.elminoCaracterDeLista(generos,caracteresComplejos)
         tabla_para_plot = genero_tabla_para_plot(filtro,generos)
         barplot(tabla_para_plot)
